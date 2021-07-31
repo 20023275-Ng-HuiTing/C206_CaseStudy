dea6132cdabdbe2693539faa8cf1f35e2409e1f1
20023275 <20023275@LAPTOP-VPRG676P>
2021-07-31 19:20:54 +0800

    Created foodItem class - Hui Ting

---
+++ C206_CaseStudy/src/foodItem.java
@@ -0,0 +1,11 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ * 20023275, 31 Jul 2021 6:41:14 pm
+ */
+
+public class foodItem {
+
+}
