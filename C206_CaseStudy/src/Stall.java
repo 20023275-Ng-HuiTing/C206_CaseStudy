ba7cb350421b792fb3a4cfc437b65cc63f8a169b
20023275 <20023275@LAPTOP-VPRG676P>
2021-08-02 19:32:56 +0800

    Merged Stall.java with the src - Hui Ting

--- C206_CaseStudy/src/Stall.java
+++ C206_CaseStudy/src/Stall.java
@@ -1,10 +1,4 @@
 // Gary
-/**
- * I declare that this code was written by me.
- * I will not copy or allow others to copy my code.
- * I understand that copying code is considered as plagiarism.
- * 20023275, 1 Aug 2021 12:51:02 pm
- */
 
 public class Stall {
 	private String StallName;
